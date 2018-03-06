082a19514b2ac7c9989561102766658ebdba9e4c
alexandreMartinEcl <alexandre.martin.ecl@gmail.com>
2018-03-06 22:59:00 +0100

    Created CodePages, added to Content and updated tabbar to be more personnalised

--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -1,8 +1,25 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
-//import {Button} from 'material-ui';
+import { withStyles } from 'material-ui';
 
 import IdentityResp from './IdentityResp';
+import CodePages from './CodePages';
+
+const styles = theme => ({
+  root: {
+    marginTop: 56,
+    '@media (min-width:0px) and (orientation: landscape)': {
+      marginTop: 48
+    },
+    '@media (min-width:600px)': {
+      marginTop: 64
+    }
+  },
+  content: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit * 5,
+  }
+});
 
 class Content extends PureComponent {
   static propTypes = {
@@ -14,17 +31,38 @@ class Content extends PureComponent {
   };
 
   state = {
-    name: 'Albert',
-  }
+    sessionId: "",
+    users: [],
+    htmlTxt: "",
+    cssTxt: "",
+    jsTxt: "",
+  };
 
+  openSession = (htmlTxt, cssTxt, jsTxt, sessionId, users) => {
+    console.log("Session opened" + " (html: " + htmlTxt + ", css: " + cssTxt + ", js: " + jsTxt + ", sessionId: " + sessionId);
+    this.setState({htmlTxt, cssTxt, jsTxt, sessionId, users});
+  };
 
   render() {
-    return (
-      <div className={this.props.className}>
-        <IdentityResp />
-      </div>
-    )
+    if (this.state.sessionId.length > 0){
+      return (
+        <div className={this.props.className}>
+          Session {this.state.sessionId}
+          <CodePages 
+          htmlTxt={this.state.htmlTxt}
+          cssTxt={this.state.cssTxt}
+          jsTxt={this.state.jsTxt} 
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div className={this.props.className}>
+          <IdentityResp sessionFunc={this.openSession} />
+        </div>
+      );
+    }
   }
 }
 
-export default Content;
+export default withStyles(styles)(Content);
