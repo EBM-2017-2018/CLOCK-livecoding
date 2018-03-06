27797c1cc6617801607299f2b70868a37caf4337
alexandreMartinEcl <alexandre.martin.ecl@gmail.com>
2018-03-06 22:37:29 +0100

    finished an operationnal version of Checking identity and getting a session (local, onlineversion commented)

--- frontend/src/components/IdentityResp.jsx
+++ frontend/src/components/IdentityResp.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
+import { withStyles } from 'material-ui';
 
 import { backendCheckUser } from '../repository/user.repository';
 import PropTypes from 'prop-types';
 import IsOk from "./IsOk";
 import IsNotOk from "./IsNotOk";
+import UserInterface from "./UserInterface";
+
+const styles = theme => ({
+  root: {
+    marginTop: 56,
+    '@media (min-width:0px) and (orientation: landscape)': {
+      marginTop: 48
+    },
+    '@media (min-width:600px)': {
+      marginTop: 64
+    }
+  },
+  content: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit * 5,
+  }
+});
 
 class IdentityResp extends React.PureComponent {
   static propTypes = {
     userData: PropTypes.shape({
       name: PropTypes.string,
       authentified: PropTypes.bool
-    })
+    }),
+    sessionFunc: PropTypes.func
   };
 
   state = {
@@ -18,7 +37,8 @@ class IdentityResp extends React.PureComponent {
       name: ""
     },
     authentified: false,
-    requested: false
+    requested: false,
+    sessionId: ""
   }
 
   componentWillMount() {
@@ -32,18 +52,24 @@ class IdentityResp extends React.PureComponent {
   }
 
   render() {
-    if(!this.state.requested){
-      return (
-      <div>Checking User</div>
+    if(this.state.sessionId.length > 0){
+      return(
+        <UserInterface />
       );
-    }
-    if(this.state.authentified){
-      return <div>{this.userData.name}<IsOk/></div>;
-    }
-    else{
-      return <IsNotOk/>;
+    } else {
+      if(!this.state.requested){
+        return (
+        <div>Checking User</div>
+        );
+      }
+
+      if(this.state.authentified){
+        return <IsOk name={this.state.userData.name} contentOpenSession={this.props.sessionFunc} />;
+      } else {
+        return <IsNotOk/>;
+      }  
     }
   }
 }
 
-export default IdentityResp;
+export default withStyles(styles)(IdentityResp);
