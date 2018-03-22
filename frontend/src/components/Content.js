b176964aa33a9ee876860352f3f5ce902001393b
alexandreMartinEcl <alexandre.martin.ecl@gmail.com>
2018-03-22 09:05:38 +0100

    --amend

--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -136,7 +136,7 @@ class Content extends PureComponent {
     console.log(`Session opened (html: ${code.html}, css: ${code.css}, js: ${code.js}, sessionHash: ${hash}, sessionName: ${name}, users: `);
     console.log(users);
 
-    this.addUserCode(this.state.currentUser.username, "Mon espace", html, code.css, code.js);
+    this.addUserCode(this.state.currentUser.username, "Mon espace", code.html, code.css, code.js);
 
     this.setState({session: {opened: true, hash, name}, users: users});
   };
